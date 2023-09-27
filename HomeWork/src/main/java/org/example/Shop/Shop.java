3bae6871398e135468868ea16135b3262e89f1e9
Demetrius81 <shizimaniak@gmail.com>
2023-09-27 21:16:58 +0300

    hw1 task2 in progress

--- HomeWork/src/main/java/org/example/Shop/Shop.java
+++ HomeWork/src/main/java/org/example/Shop/Shop.java
@@ -1,6 +1,6 @@
-package seminars.first.Shop;
+package org.example.Shop;
 
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.List;
 
 public class Shop {
@@ -18,13 +18,27 @@ public class Shop {
     // Метод должен вернуть отсортированный по возрастанию по цене список продуктов
     public List<Product> sortProductsByPrice() {
         // Допишите реализацию метода самостоятельно
-        return null;
+        Product[] productsArr = new Product[getProducts().size()];
+        getProducts().toArray(productsArr);
+        sortArr(productsArr);
+        return Arrays.stream(productsArr).toList();
     }
 
     // Метод должен вернуть самый дорогой продукт
     public Product getMostExpensiveProduct() {
         // Допишите реализацию метода самостоятельно
-        return null;
+        return sortProductsByPrice().get(products.size() - 1);
+    }
+
+    private static void sortArr(Product[] sortArr) {
+        int j;
+        for (int i = 1; i < sortArr.length; i++) {
+            Product swap = sortArr[i];
+            for (j = i; j > 0 && swap.compareCost(sortArr[j - 1]) < 0; j--) {
+                sortArr[j] = sortArr[j - 1];
+            }
+            sortArr[j] = swap;
+        }
     }
 
 }
