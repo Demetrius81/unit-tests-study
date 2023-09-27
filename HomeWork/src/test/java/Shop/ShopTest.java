1b0488fe33186203c779092a8a0a5ddf3007e4e5
Demetrius81 <shizimaniak@gmail.com>
2023-09-27 22:23:18 +0300

    task 2 in progress yet

--- HomeWork/src/test/java/Shop/ShopTest.java
+++ HomeWork/src/test/java/Shop/ShopTest.java
@@ -17,82 +17,86 @@ public class ShopTest {
    3. Напишите тесты для проверки корректности работы метода sortProductsByPrice (проверьте правильность сортировки).
    */
 
+    private static Shop _shop;
+    private static int _count;
+    private static Product[] _products;
 
-    @Test
-    void getProducts_CorrectList_IsCorrectStorageTest(){
-        Shop shop = new Shop();
+    public ShopTest() {
+        this._shop = new Shop();
         Random rnd = new Random();
-        int count = 100;
-        Product[] products = new Product[count];
+        this._count = 100;
+        this._products = new Product[_count];
 
-        for (int i = 1; i <= count; i++){
+        for (int i = 1; i <= this._count; i++) {
             String title = "product" + i;
             Product p = new Product();
             p.setTitle(title);
             p.setCost(1 + rnd.nextInt(100));
-            products[i - 1] = p;
+            this._products[i - 1] = p;
         }
 
-        shop.setProducts(Arrays.stream(products).toList());
+        this._shop.setProducts(Arrays.stream(this._products).toList());
 
     }
 
-    public static void main(String[] args) {
-    /*
-   1. Напишите тесты, чтобы проверить, что магазин хранит верный список продуктов (правильное количество продуктов, верное содержимое корзины).
-   2. Напишите тесты для проверки корректности работы метода getMostExpensiveProduct.
-   3. Напишите тесты для проверки корректности работы метода sortProductsByPrice (проверьте правильность сортировки).
-   */
-        Shop shop = new Shop();
-        Random rnd = new Random();
-        int count = 100;
-        Product[] products = new Product[count];
-
-        for (int i = 1; i <= count; i++){
-            String title = "product" + i;
-            Product p = new Product();
-            p.setTitle(title);
-            p.setCost(1 + rnd.nextInt(100));
-            products[i - 1] = p;
-        }
-
-        shop.setProducts(Arrays.stream(products).toList());
+    @Test
+    void getProducts_CorrectShop_IsCorrectStorageTest() {
 
         // Напишите тесты, чтобы проверить,
-        // что магазин хранит верный список продуктов (правильное количество продуктов, верное содержимое корзины).
+        // что магазин хранит не пустой список продуктов
 
-        assertThat(shop.getProducts().isEmpty()).isEqualTo(false);
-        assertThat(shop.getProducts().size()).isEqualTo(count);
-        assertThat(shop.getProducts().get(shop.getProducts().size() - 1).getClass()).isEqualTo(new Product().getClass());
+        assertThat(_shop.getProducts())
+                .describedAs("The product list should not be empty")
+                .isNotEmpty();
 
+        // что магазин хранит правильное количество продуктов
+        assertThat(_shop.getProducts())
+                .describedAs("The product list should size should be %d", _count)
+                .hasSize(_count);
+
+        // что магазин хранит верное содержимое корзины
+        assertThat(_shop
+                .getProducts()
+                .get(_shop
+                        .getProducts()
+                        .size() - 1))
+                .describedAs("The product list should be instance of Product class")
+                .isInstanceOf(Product.class);
+    }
+
+    @Test
+    void getMostExpensiveProduct_CorrectShop_IsCorrectStorageTest() {
         // Напишите тесты для проверки корректности работы метода getMostExpensiveProduct.
 
         int max = 0;
 
-        for (Product p:
-                products) {
+        for (Product p :
+                this._products) {
             if (max < p.getCost()) {
                 max = p.getCost();
             }
         }
-        assertThat(shop.getMostExpensiveProduct().getCost()).isEqualTo(max);
+        assertThat(this._shop.getMostExpensiveProduct().getCost()).describedAs("This method must return product with highest price").isEqualTo(max);
+    }
 
+    @Test
+    void sortProductsByPrice_CorrectShop_IsCorrectSortedStorageTest() {
         // Напишите тесты для проверки корректности работы метода sortProductsByPrice (проверьте правильность сортировки).
 
-        int[] costs = new int[count];
+        int[] costs = new int[this._count];
 
-        for (int i = 0; i < products.length; i++){
-            costs[i] = products[i].getCost();
+        for (int i = 0; i < this._products.length; i++) {
+            costs[i] = this._products[i].getCost();
         }
 
-        costs = Arrays.stream(costs).sorted().toArray();
+        Arrays.sort(costs);
 
         int i = 0;
 
-        for (Product p:
-                shop.sortProductsByPrice()) {
+        for (Product p :
+                this._shop.sortProductsByPrice()) {
             assertThat(p.getCost()).isEqualTo(costs[i]);
             i++;
         }
-
+    }
 }
