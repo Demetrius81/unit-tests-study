49ba286afbc053c7448c6d7ba24d0ee883adc3de
Demetrius81 <shizimaniak@gmail.com>
2023-10-16 14:38:52 +0300

    homeWork is ready

--- lesson6/src/main/java/org/example/Main.java
+++ lesson6/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+
+import java.io.File;
+import java.security.Permission;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
